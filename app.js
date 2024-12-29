6f7e4b98059545697ef9c1a48d33dc4299e97bc4
ITZARYAN18 <aryan0901k@gmail.com>
2024-12-29 11:49:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ const updateTasksList = () => {
                     <p>${task.text}</p>
                 </div>
                 <div class="icons">
-                    <img src="./img/edit.png" alt="Edit" onClick="editTask(${index})"/>
-                    <img src="./img/bin.png" alt="Delete" onClick="deleteTask(${index})"/>
+                    <img src="edit.png" alt="Edit" onClick="editTask(${index})"/>
+                    <img src="bin.png" alt="Delete" onClick="deleteTask(${index})"/>
                 </div>
             </div>
         `;
